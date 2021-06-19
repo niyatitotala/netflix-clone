5d072d5bc541ac63ec4680681ea5e6615a9186c2
niyatitotala <niyatimetotala@gmail.com>
2021-06-19 19:59:29 +0530

    Complete React Code

---
+++ src/foobar.js
@@ -0,0 +1,3 @@
+// 2ec840c6536457340ba67270100d37d6 - v3 auth API KEY
+// https://api.themoviedb.org/3/movie/550?api_key=2ec840c6536457340ba67270100d37d6 Example of API Req 
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZWM4NDBjNjUzNjQ1NzM0MGJhNjcyNzAxMDBkMzdkNiIsInN1YiI6IjYwYzVmNTM2NDdjOWZiMDAyOWUwYjRlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FNX1oohPpChvhATF3xDt2Rg9aTC_cKSS3275psN2uSo v4 auth token
---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "2ec840c6536457340ba67270100d37d6";
+
+const requests = {
+    fetchPopular: `/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
